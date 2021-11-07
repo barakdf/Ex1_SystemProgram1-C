7a1172fc34c09835b11f4eabc472c267f9da0bde
barakdf <barakdf@gmail.com>
2021-11-07 19:29:26 +0200

    testKAPARA

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -5,28 +5,32 @@
 int isPalindrome(int n);
 int isArmstrong(int n);
 
-int nth(int x) {
+int nth(int x)
+{
     int n = 0;
-    while(x != 0){
+    while (x != 0)
+    {
         n++;
         x = x / 10;
     }
     return n;
 }
 
-
-
-double isArmstrongRec(int n, int len){
-    if (n == 0) {
+double isArmstrongRec(int n, int len)
+{
+    if (n == 0)
+    {
         return 0;
     }
-    double curr = pow(n%10, len);
-    return curr + isArmstrongRec(n/10,len);
+    double curr = pow(n % 10, len);
+    return curr + isArmstrongRec(n / 10, len);
 }
 
-int isArmstrong(int n) {
+int isArmstrong(int n)
+{
     int len = nth(n);
-    if(isArmstrongRec(n,len) == n) {
+    if (isArmstrongRec(n, len) == n)
+    {
         return 1;
     }
     return 0;
@@ -38,14 +42,13 @@ int isPalindrome(int num)
      * Check if the given number is equal to 
      * its reverse.
      */
-    if(num == reverse(num))
+    if (num == reverse(num))
     {
         return 1;
     }
-    
+
     return 0;
 }
-
 
 /**
  * Recursive function to find reverse of any number
@@ -54,10 +57,10 @@ int reverse(int num)
 {
     /* Find number of digits in num */
     int digit = (int)log10(num);
-    
+
     /* Recursion base condition */
-    if(num == 0)
+    if (num == 0)
         return 0;
 
-    return ((num%10 * pow(10, digit)) + reverse(num/10));
+    return ((num % 10 * pow(10, digit)) + reverse(num / 10));
 }
