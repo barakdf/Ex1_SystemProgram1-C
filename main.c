be4a4e98d4fb77bd518747e45de112ca0ba435a7
barakdf <barakdf@gmail.com>
2021-11-07 18:20:35 +0200

    Test_1

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 int main() {
     int a, b;
     printf("please enter the First and the Second numbers: ");
-    scanf("%d %d", &a, &b);
+    scanf("%d", &a);
+	scanf("%d", &b);
 	if (b < a)
 	{
 		int temp = a;
